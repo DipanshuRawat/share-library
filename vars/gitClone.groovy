ff01fa3c05930f4f65bcc3244fdcd1e5c7e1c8ef
DipanshuRawat <110223176+DipanshuRawat@users.noreply.github.com>
2025-03-18 01:18:52 +0530

    Update gitClone.groovy

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -1,6 +1,10 @@
 // vars/gitClone.groovy
 
-def call(String repoUrl, String branchName = 'main', String credentialsId = 'Cred') {
+def call(Map config) {
+    def repoUrl = config.repoUrl
+    def branchName = config.branchName ?: 'main' // Default to 'main' if branchName is not provided
+    def credentialsId = config.credentialsId
+
     stage('Git Clone') {
         checkout([
             $class: 'GitSCM',
