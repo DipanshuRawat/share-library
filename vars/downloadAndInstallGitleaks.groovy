9233af2735a25d6dd655540f3193174d7207fd56
DipanshuRawat <110223176+DipanshuRawat@users.noreply.github.com>
2025-03-18 00:39:00 +0530

    Update and rename gitLeaks.groovy to downloadAndInstallGitleaks.groovy

--- vars/downloadAndInstallGitleaks.groovy
+++ vars/downloadAndInstallGitleaks.groovy
@@ -1,6 +1,6 @@
-// vars/gitLeaks.groovy
+// vars/downloadAndInstallGitleaks.groovy
 
-def call(String gitleaksReportName = "gitleaks-report.json") {
+def call() {
     stage('Download and Install Gitleaks') {
         sh '''
             wget https://github.com/gitleaks/gitleaks/releases/download/v8.24.0/gitleaks_8.24.0_linux_x64.tar.gz
@@ -8,13 +8,5 @@ def call(String gitleaksReportName = "gitleaks-report.json") {
             chmod +x gitleaks
             sudo mv gitleaks /usr/local/bin/
         '''
-    }
-
-    stage('Gitleaks Scan') {
-        sh "gitleaks detect -r ${gitleaksReportName}"
-    }
-
-    stage('Publish Gitleaks Report') {
-        archiveArtifacts artifacts: gitleaksReportName, fingerprint: true
     }
 }
