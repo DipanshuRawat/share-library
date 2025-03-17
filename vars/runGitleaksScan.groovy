19a7cee8aaed6199d254ff5b17c96ffd358b543d
DipanshuRawat <110223176+DipanshuRawat@users.noreply.github.com>
2025-03-18 01:24:33 +0530

    Update runGitleaksScan.groovy

--- vars/runGitleaksScan.groovy
+++ vars/runGitleaksScan.groovy
@@ -1,6 +1,8 @@
 // vars/runGitleaksScan.groovy
 
-def call(String gitleaksReportName = "gitleaks-report.json") {
+def call(Map config) {
+    def gitleaksReportName = config.gitleaksReportName ?: "gitleaks-report.json" // Default report name
+
     stage('Gitleaks Scan') {
         sh "gitleaks detect -r ${gitleaksReportName}"
     }
